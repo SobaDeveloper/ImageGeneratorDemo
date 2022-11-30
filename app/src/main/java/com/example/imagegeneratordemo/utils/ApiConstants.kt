f5ae637f5d6be5092d32cfb057ce3b72ddf42f4e
Soba Developer <levi.hsiao@gmail.com>
2022-11-30 03:30:58 -0800

    Update API key

--- app/src/main/java/com/example/imagegeneratordemo/utils/ApiConstants.kt
+++ app/src/main/java/com/example/imagegeneratordemo/utils/ApiConstants.kt
@@ -3,7 +3,7 @@ package com.example.imagegeneratordemo.utils
 object ApiConstants {
     const val BASE_URL = "https://api.openai.com/v1/"
 
-    const val API_KEY = " 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"
+    const val API_KEY = "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"
     const val KEY_HEADER_AUTHORIZATION = "Authorization"
     const val KEY_HEADER_CONTENT_TYPE = "Content-Type"
     const val VALUE_HEADER_BEARER = "Bearer $API_KEY"
